1e0227db107daa10993db132d770ac6d797ab815
david30-maker <davidokpah396@gmail.com>
2023-11-08 21:07:45 +0100

    Implement index query on instructor table

--- schema.sql
+++ schema.sql
@@ -1,2 +1,7 @@
 CREATE INDEX idx_user_id ON users(id);
 CREATE INDEX idx_user_email ON users(email);
+
+CREATE INDEX idx_instructor_id ON instructors(id);
+CREATE INDEX idx_instructor_email ON instructors(email);
+
+CREATE INDEX idx_course_id ON courses(id);

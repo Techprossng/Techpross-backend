5eaff35aa1bc0a57300b05d9d74ffb4acef0fa50
david30-maker <davidokpah396@gmail.com>
2023-11-09 16:03:16 +0100

    Update requested change

--- schema.sql
+++ schema.sql
@@ -74,7 +74,7 @@ CREATE INDEX instructor_email_index ON instructors(email);
 CREATE INDEX course_id_index ON courses(id);
 CREATE INDEX instructor_id_index ON courses (instructor_id);
 
-CREATE INDEX module_id_index ON moduless(id);
-CREATE INDEX course_id_index ON moduless (course_id);
+CREATE INDEX module_id_index ON modules(id);
+CREATE INDEX course_id_index ON modules(course_id);
 
-CREATE INDEX user_id_index ON users_courses (user_id);
+CREATE INDEX user_id_index ON users_courses(user_id);

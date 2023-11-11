6501cad4d7eebeac9f85c6f90f85c90be8f69200
Taiwo Babalola Peter <babalolataiwop@gmail.com>
2023-11-11 02:59:56 +0100

    fix: database migration error with date

--- database/migrations/20231109103527_users_courses_table.js
+++ database/migrations/20231109103527_users_courses_table.js
@@ -7,12 +7,12 @@ const courses = require('./20231109103424_courses_table');
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
+exports.up = function (knex) {
   return knex.schema
-    .createTable('users_courses', function(table) {
+    .createTable('users_courses', function (table) {
       table.bigint('user_id').notNullable();
       table.bigint('course_id').notNullable();
-      table.date('date_enrolled').notNullable().defaultTo(knex.fn.now());
+      table.datetime('date_enrolled').notNullable().defaultTo(knex.fn.now());
       table.primary(['user_id', 'course_id']);
       table.foreign('user_id').references('users.id').onDelete('CASCADE');
       table.foreign('course_id').references('courses.id').onDelete('CASCADE');
@@ -24,7 +24,7 @@ exports.up = function(knex) {
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
+exports.down = function (knex) {
   return knex.schema.dropTableIfExists('users_courses');
 };
 

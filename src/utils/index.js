77d48d9129614098b52a45484a77ec6528900e13
Taiwo Babalola Peter <babalolataiwop@gmail.com>
2023-11-12 14:47:31 +0100

    [add]: email checker for validation

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,8 +4,11 @@ const { hashPassword, verifyPassword } = require('./password');
  * ### helper utilities
  */
 exports.Util = {
-    hashPassword: hashPassword,
-    verifyPassword: verifyPassword,
-    checkDigit: /^[0-9]+$/,
-    getOffset: (pageNum, limit) => pageNum > 0 ? (pageNum - 1) * limit : 0
+    hashPassword,
+    verifyPassword,
+    checkDigit: (digit) => /^[0-9]+$/.test(digit),
+    checkIsEmail: (email) => {
+        return /^[0-9a-zA-Z_+.%]+@[A-Za-z0-9.-_]+\.[A-Za-z.]{2,4}$/.test(email)
+    },
+    getOffset: (pageNum, limit) => pageNum > 0 ? (pageNum - 1) * limit : 0,
 }

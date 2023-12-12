037db91a841ad1625d09f990cec5c16a643a9e85
Mistaweng <emmanuelgyang.1822@gmail.com>
2023-12-12 02:41:26 +0100

    Added the protected routes

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,39 +1,40 @@
 const User = require('../models/User');
-const { createAccessToken, createRefreshToken, verifyPassword } = require('../middlewares/token');
-const { encryptPassword } = require('../utils/util');
+const { createAccessToken, createRefreshToken, verifyAccessToken } = require('../middlewares/token');
+const { encryptPassword, comparePasswords } = require('../utils/util');
+const cookieParser = require('cookie-parser');
 
 class UserController {
   static async register(request, response) {
     try {
-      const { email, password } = request.body;
+        const { email, password } = request.body;
 
-      // Validate email and password
-      if (!this.validateEmail(email) || !this.validatePassword(password)) {
-        return response.status(400).json({ error: 'Invalid email or password format' });
-      }
-
-      // Check if email already exists
-      const existingUser = await User.query().findOne({ email });
-      if (existingUser) {
-        return response.status(400).json({ error: 'Email already registered' });
-      }
-
-      // Encrypt password
-      const hashedPassword = await encryptPassword(password);
-
-      // Create a new user
-      const newUser = await User.query().insert({ email, password: hashedPassword });
-
-      // Create tokens
-      const accessToken = await createAccessToken(email);
-      const refreshToken = await createRefreshToken(email);
-
-      return response.status(201).json({
-        message: 'Registration successful',
-        accessToken,
-        refreshToken,
-        ...newUser,
-      });
+        // Validate email and password
+        if (!this.validateEmail(email) || !this.validatePassword(password)) {
+          return response.status(400).json({ error: 'Invalid email or password format' });
+        }
+  
+        // Check if email already exists
+        const existingUser = await User.query().findOne({ email });
+        if (existingUser) {
+          return response.status(400).json({ error: 'Email already registered' });
+        }
+  
+        // Encrypt password
+        const hashedPassword = await encryptPassword(password);
+  
+        // Create a new user
+        const newUser = await User.query().insert({ email, password: hashedPassword });
+  
+        // Create tokens
+        const accessToken = await createAccessToken(email);
+        const refreshToken = await createRefreshToken(email);
+  
+        return response.status(201).json({
+          message: 'Registration successful',
+          accessToken,
+          refreshToken,
+          ...newUser,
+        });
     } catch (error) {
       console.error(error);
       return response.status(500).json({ error: 'Internal server error' });
@@ -42,23 +43,27 @@ class UserController {
 
   static async login(request, response) {
     try {
-      const { email, password } = request.body;
+        const { email, password } = request.body;
 
-      // Verify user
-      const user = await User.query().findOne({ email });
-      if (!user) {
-        return response.status(400).json({ error: 'User not found' });
-      }
+        // Verify user
+        const user = await User.query().findOne({ email });
+        if (!user) {
+          return response.status(400).json({ error: 'User not found' });
+        }
+  
+        // Verify password
+        const isMatch = await verifyPassword(password, user.password);
+        if (!isMatch) {
+          return response.status(400).json({ error: 'Wrong password' });
+        }
+  
+        // Create tokens
+        const accessToken = await createAccessToken(email);
+        const refreshToken = await createRefreshToken(email);
 
-      // Verify password
-      const isMatch = await verifyPassword(password, user.password);
-      if (!isMatch) {
-        return response.status(400).json({ error: 'Wrong password' });
-      }
-
-      // Create tokens
-      const accessToken = await createAccessToken(email);
-      const refreshToken = await createRefreshToken(email);
+      // Set cookies
+      response.cookie('access_token', accessToken, { httpOnly: true });
+      response.cookie('refresh_token', refreshToken, { httpOnly: true });
 
       return response.status(200).json({
         message: 'Login successful',
@@ -72,14 +77,33 @@ class UserController {
     }
   }
 
-  // Validation methods
-  static validateEmail(email) {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-  }
+  static async protectedRoute(request, response) {
+    try {
+      // Access token from cookie
+      const accessToken = request.cookies.access_token;
+      
+      // Verify the access token and get decoded token data
+      const decodedToken = await verifyAccessToken(accessToken);
 
-  static validatePassword(password) {
-    return password.length >= 8;
+      // Access decoded token data
+      console.log(decodedToken);
+
+      // User model has method to find a user by ID
+      const userId = decodedToken.id; 
+      const user = await User.query().findById(userId);
+
+      return response.status(200).json({
+        message: 'Protected route accessed',
+        user: {
+          id: user.id,
+          email: user.email,
+          // User details as needed can be added here
+        },
+      });
+    } catch (error) {
+      console.error(error);
+      return response.status(401).json({ error: 'Unauthorized' });
+    }
   }
 }
 
